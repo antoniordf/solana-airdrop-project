e5889f35f8ff275d0b0600666aa609589d9e2ca2
Antonio Furtado <antoniordf@hotmail.com>
2023-12-08 17:20:54 +0000

    airdrop

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const {
-  connection,
+  Connection,
   PublicKey,
   clusterApiUrl,
   Keypair,
@@ -7,3 +7,37 @@ const {
 } = require("@solana/web3.js");
 
 const wallet = new Keypair();
+
+const publicKey = new PublicKey(wallet._keypair.publicKey);
+const secretKey = wallet._keypair.secretKey;
+
+const getWalletBalance = async () => {
+  try {
+    const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+    const walletBalance = await connection.getBalance(publicKey);
+    console.log(`Wallet Balance: ${walletBalance}`);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const airDropSol = async () => {
+  try {
+    const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+    const fromAirDropSignature = await connection.requestAirdrop(
+      publicKey,
+      2 * LAMPORTS_PER_SOL
+    );
+    await connection.confirmTransaction(fromAirDropSignature);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const main = async () => {
+  await getWalletBalance();
+  await airDropSol();
+  await getWalletBalance();
+};
+
+main();
